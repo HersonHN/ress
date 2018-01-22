26ec1a0b4d08f0dbc00398baca503a4dc5c85800
Herson Salinas <hersonhn@gmail.com>
2018-01-22 02:03:51 -0600

    add target="_blank" to links on articles

--- server/scripts/clean-view.js
+++ server/scripts/clean-view.js
@@ -12,7 +12,7 @@ function cleanView(url) {
   readability(url, function (err, article) {
     if (err) return deferred.reject(err);
 
-    let content = article.content;
+    let content = extraSanitize(article.content);
     article.close();
     deferred.resolve(content);
   })
@@ -21,11 +21,16 @@ function cleanView(url) {
 }
 
 
+function extraSanitize(content) {
+  return content.replace(/<a/g, '<a target="_blank"');
+}
+
+
 function main() {
   if (require.main === module) {
     let defaultURL = 'https://opinionator.blogs.nytimes.com/2015/01/11/why-life-is-absurd/';
     let param = process.argv[2];
-    
+
     cleanView(param || defaultURL).then(function (output) {
       console.log(output);
     });
