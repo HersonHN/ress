123928d40383d3e3b99674893756ba9e89e55620
Herson Salinas <hersonhn@gmail.com>
2018-09-25 01:31:13 -0600

    vue instance fix

--- src/main.js
+++ src/main.js
@@ -10,10 +10,7 @@ Vue.config.productionTip = false
 
 Vue.filter('date', DateFilter);
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
