945855a149c9f402c1a6e4ee1ff0537f1a01c24d
Herson Salinas <hersonhn@gmail.com>
2018-10-25 02:09:00 -0600

    native scrollTo function

--- src/helpers/ui.js
+++ src/helpers/ui.js
@@ -1,19 +1,10 @@
 import mini from './mini-jquery';
 
-function scrollTo(element, duration) {
-  duration = duration || 300;
-  let top = element.offsetTop;
-
-  let scrollStep = -window.scrollY / (duration / 15);
-  let scrollInterval = setInterval(function () {
-      
-      if (window.scrollY > top) {
-        window.scrollBy(top, scrollStep);
-      } else {
-        clearInterval(scrollInterval);
-      }
-  }, 15);
-
+function scrollTo(element) {
+  element.scrollIntoView({ 
+    block: 'start',
+    behavior: 'smooth'
+  });
 }
 
 function highlight(element) {
