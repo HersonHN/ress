6bf02d50030f857eefb4bda7a954fb84d7c5dbc4
Herson Salinas <hersonhn@gmail.com>
2020-04-04 14:20:26 -0600

    assets in the assets dir

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,19 @@
 const path = require('path');
 
 let webpackConfig = {
+  // optimization: {
+  //   minimize: false
+  // }
 };
 
 module.exports = {
   configureWebpack: webpackConfig,
+  assetsDir: 'assets',
+  productionSourceMap: false,
+  filenameHashing: true,
   css: {
     loaderOptions: {
       sass: {
-        // implementation: require('sass'),
         sassOptions: {
           includePaths: [
             path.resolve(__dirname, 'node_modules')
