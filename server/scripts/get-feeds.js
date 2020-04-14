8878c4513b2d8ce4fd28b81e4d2508df5cc8091a
Herson Salinas <hersonhn@gmail.com>
2020-04-13 23:08:07 -0600

    Loading from cache (if needed) on custom hompeage

--- server/scripts/get-feeds.js
+++ server/scripts/get-feeds.js
@@ -1,14 +1,49 @@
 
 const rssParser = require('./rss-parser');
 
-module.exports = function (req, res) {
+module.exports = {
+  single,
+  homepage,
+  homePageCustom,
+}
+
+function single(req, res, cache) {
+  let content = cache.news[req.params.id];
+  if (!content) {
+    res.status(500)
+      .send({ error: 'Invalid feed id' });
+    return;
+  }
+  res.send(content)
+}
+
+
+function homepage(req, res, cache) {
+  return res.send(cache.news);
+}
+
+
+function homePageCustom(req, res, cache) {
   let feeds = req.body.feeds;
 
   if (feeds.length > 7) {
     return res.status(413).send({ error: 'Payload Too Large'})
   }
 
-  rssParser.getRSS(feeds)
+  let cached = [];
+  let notCached = [];
+
+  // there's no need to load again already cached feeds
+  cached = feeds.filter(feed => !!cache.news[feed.id]);
+  cached = cached.map(feed => Promise.resolve(cache.news[feed.id]));
+
+  // the not cached feed are loaded with the rss parser
+  notCached = feeds.filter(feed => !cache.news[feed.id]);
+  notCached = notCached.map(feed => rssParser.parseSingleFeed(feed));
+
+  // both groups are handled by the same promise
+  Promise.all([...cached, ...notCached])
+    .then(feeds => rssParser.mergeFeeds(feeds))
     .then(content => res.json(content))
     .catch(e => console.error(e));
 };
