727d04324923c3b0749ddc3c9a4a3b0ed019968c
Herson Salinas <hersonhn@gmail.com>
2020-04-13 19:27:12 -0600

    cleaning server index.js

--- index.js
+++ index.js
@@ -3,22 +3,19 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
 
-const rssParser = require('./server/scripts/rss-parser');
+const ServerLoop = require('./server/server-loop');
+const cleanPage = require('./server/scripts/clean');
 const sources = require('./sources.json');
 const config = require('./server-config');
 
-const server = express();
 const publicPath = path.join(__dirname, 'dist');
-console.log('publicPath', publicPath);
 
-const CACHE = {
-  news: {}
-};
+const server = express();
+const loop = new ServerLoop({ server, config });
 
 
-server.use(function(req, res, next) {
+server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -29,96 +26,23 @@ server.use(bodyParser.json());
 server.use(express.static(publicPath));
 
 
-// rendering the index
-server.get('/feed/:feedId', function (req, res) {
-  res.sendFile('index.html', { root: publicPath });
-});
+// rendering the index for each feed
+server.get('/feed/:feedId', (req, res) => res.sendFile('index.html', { root: publicPath }));
 
+server.get('/api/sources',  (req, res) => res.send(sources));
+server.get('/api/feed',     (req, res) => res.send(loop.cache.news));
+server.get('/api/feed/:id', (req, res) => res.send(loop.cache.news[id]));
+server.post('/api/clean',   cleanPage);
 
-server.get('/api/sources', function (req, res) {
-  res.send(sources);
-});
-
-
-server.get('/api/feed', function (req, res) {
-  res.send(CACHE.news);
-});
-
-
-server.get('/api/feed/:id', function (req, res) {
-  let id = req.params.id;
-  let sourceExists = CACHE.news[id];
-
-  if (!sourceExists) {
-    return res
-      .status(500)
-      .send({ error: 'Invalid feed id' });
-  }
-
-  res.send(CACHE.news[id]);
-});
-
-
-server.post('/api/clean', function (req, res) {
-  let url = req.body.url;
-
-  axios.get(url, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.2; +http://www.google.com/bot.html)'
-      }
-    })
-    .then(res => res.data)
-    .then(function (text) {
-      res.send(text);
-    })
-    .catch(function (error) {
-      console.error(error);
-      res.status(500).send('<strong class="error">Cannot read page<strong>');
-    });
-
-});
-
-
-function init() {
-  setInterval(getNews, config.updateInterval);
-
-  return getNews(1)
-    .then(function () {
-      server.listen(config.port, function () {
-        console.log(`Server running at port ${config.port}`);
-      });
-    });
-}
-
-
-function getNews(fistTime) {
-  if (!fistTime) console.log('Updating news');
-
-  return rssParser.getRSS()
-    .then(allFeeds => {
-
-      if (fistTime) {
-        CACHE.news = allFeeds;
-        return;
-      }
-
-      Object.entries(allFeeds).forEach(([key, entry]) => {
-          // only copy the feeds with entries
-          // in case there was an error reading the rss, the entry.feed comes as an empty array.
-          if (entry.feed && entry.feed.length) {
-            CACHE.news[key] = entry;
-          }
-        })
-    })
-    .catch(console.error);
-}
 
 
 if (require.main === module || process.env.NODE_ENV == 'production') {
-  init();
+  loop.init();
   module.exports = { already: true };
 } else {
-  module.exports = { init };
+  module.exports = {
+    init: () => loop.init()
+  };
 }
 
 
